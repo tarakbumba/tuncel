a4fd1c144ac7325a11b597f46689ebd7486ef05c
tarakbumba <tarakbumba@gmail.com>
2013-12-30 01:17:21 +0200

    - Add changelog generation script to maintainers : gitlog-to-changelog.sh - Add automatic generation of tuncel.pot script: makepot - Combine git-to-changelog script and desktop_translations script into one script : maintain_jobs.sh

---
+++ maintainers/maintain_jobs.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env sh
+update_desktop () {
+directories="actions kde"
+for i in $directories; do
+    echo "Entering into $i directory...
+"
+    cp tuncel_desktop2po.py ../"$i"/ 
+    pushd ../"$i"
+    python2 tuncel_desktop2po.py
+    mv po/tuncel_desktop.pot po/tuncel_"$i"_desktop.pot
+    rm -fr python2 tuncel_desktop2po.py
+    popd
+done
+
+}
+update_changelog() {
+pushd ../
+if test -d ".git"; then
+    git log --date-order --date=short | \
+    sed -e '/^commit.*$/d' | \
+    awk '/^Author/ {sub(/\\$/,""); getline t; print $0 t; next}; 1' | \
+    sed -e 's/^Author: //g' | \
+    sed -e 's/\(.*\)>Date: \([0-9]*-[0-9]*-[0-9]*\)/\2 \1>/g' | \
+    sed -e 's/^\(.*\) \(\)\t\(.*\)/\3    \1    \2/g' > ChangeLog
+else
+    echo "No git repository present."
+popd
+}
+
